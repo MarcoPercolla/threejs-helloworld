ee0996d65f59bbf634430c3a63c5339cdf2ae421
MarcoPercolla <percollamarco@virgilio.com>
2024-05-11 04:10:23 +0200

    add chaotic movement to particles positions

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -6,9 +6,13 @@ uniform float uTime;
 void main() {
 
   vColor = color;
-  vec3 wPos = vec4( modelMatrix * vec4(position, 1.0) ).xyz;
+  vec3 pos = position;
+  pos.y += sin(uTime * 0.5 * (offset - 0.5) + offset * 10.) * 0.05;
+  pos.x += cos(uTime * 0.5 * (offset - 0.5) + offset * 10.) * 0.05;
+  
+  vec3 wPos = vec4( modelMatrix * vec4(pos, 1.0) ).xyz;
   float dist = distance(cameraPosition, wPos);
 
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  gl_PointSize = 150. * smoothstep(15.,0., dist) * (sin(uTime * 4. + offset * 20.)*0.4 +0.6);
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+  gl_PointSize = 100. * smoothstep(15.,0., dist) * (sin(uTime * 4. + offset * 20.)*0.4 +0.6);
 }
